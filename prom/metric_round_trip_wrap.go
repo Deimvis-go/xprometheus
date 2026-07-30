a7db1fd281d6e0e6185a1b4240cf3f24ba661e15
dbrusenin <star_maps@mail.ru>
2026-07-30 23:09:28 +0300

    add new round trip wrap options

--- prom/metric_round_trip_wrap.go
+++ prom/metric_round_trip_wrap.go
@@ -4,9 +4,10 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/prometheus/client_golang/prometheus"
 	"github.com/Deimvis/go-ext/go1.25/xfallback/xfb"
 	"github.com/Deimvis/go-ext/go1.25/xhttp"
+	"github.com/Deimvis/go-ext/go1.25/xoptional"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type RoundTripWrapOption func(*roundTripWrapCfg)
@@ -22,9 +23,9 @@ func NewRoundTripWrap(ig IntervalMetricGroup, opts ...RoundTripWrapOption) xhttp
 			var resp *http.Response
 			var err error
 
-			ig.Record(cfg.newStartLabels(cfg, req), func(rc RecordControl) {
+			ig.Record(cfg.newStartLabels(&cfg, req), func(rc RecordControl) {
 				resp, err = fn(req)
-				rc.SetLabels(cfg.newFinishLabels(cfg, req, resp, err))
+				rc.SetLabels(cfg.newFinishLabels(&cfg, req, resp, err))
 			})
 
 			return resp, err
@@ -33,55 +34,83 @@ func NewRoundTripWrap(ig IntervalMetricGroup, opts ...RoundTripWrapOption) xhttp
 	}
 }
 
-func WithNormalizer(norm xhttp.PathNormalizer) RoundTripWrapOption {
+func WithStartLabelsFn(fn StartLabelsFn) RoundTripWrapOption {
 	return func(cfg *roundTripWrapCfg) {
-		cfg.norm = norm
+		cfg.newStartLabels = fn
 	}
 }
 
+func WithFinishLabelsFn(fn FinishLabelsFn) RoundTripWrapOption {
+	return func(cfg *roundTripWrapCfg) {
+		cfg.newFinishLabels = fn
+	}
+}
+
+func WithNormalizer(norm xhttp.PathNormalizer) RoundTripWrapOption {
+	return func(cfg *roundTripWrapCfg) {
+		cfg.norm.SetValue(norm)
+	}
+}
+
+type RoundTripWrapConfig interface {
+	Normalizer() (xhttp.PathNormalizer, bool)
+}
+
+type StartLabelsFn = func(RoundTripWrapConfig, *http.Request) prometheus.Labels
+type FinishLabelsFn = func(RoundTripWrapConfig, *http.Request, *http.Response, error) prometheus.Labels
+
 type roundTripWrapCfg struct {
-	norm xhttp.PathNormalizer
+	norm xoptional.T[xhttp.PathNormalizer]
 	// TODO: figure out proper interface to do not calculate labels twice
-	newStartLabels  func(roundTripWrapCfg, *http.Request) prometheus.Labels
-	newFinishLabels func(roundTripWrapCfg, *http.Request, *http.Response, error) prometheus.Labels
+	newStartLabels  StartLabelsFn
+	newFinishLabels FinishLabelsFn
+}
+
+var _ RoundTripWrapConfig = (*roundTripWrapCfg)(nil)
+
+func (cfg *roundTripWrapCfg) Normalizer() (xhttp.PathNormalizer, bool) {
+	return xoptional.ValueWithOk(cfg.norm)
 }
 
 var (
 	// start labels: host, method, path
 	// finish labels: host, method, path, code
 	defautRoundTripWrapConfig = roundTripWrapCfg{
-		newStartLabels: func(this roundTripWrapCfg, req *http.Request) prometheus.Labels {
-			ls := prometheus.Labels{
-				"host":   req.URL.Hostname(),
-				"method": req.Method,
-			}
+		norm:            xoptional.T[xhttp.PathNormalizer]{},
+		newStartLabels:  DefaultStartLabelsFn,
+		newFinishLabels: DefaultFinishLabelsFn,
+	}
+	DefaultStartLabelsFn = func(this RoundTripWrapConfig, req *http.Request) prometheus.Labels {
+		ls := prometheus.Labels{
+			"host":   req.URL.Hostname(),
+			"method": req.Method,
+		}
 
-			path := LabelUnknown
-			if this.norm != nil {
-				path = xfb.OnNilv(this.norm(req), LabelUnknown)
-			}
-			ls["path"] = path
+		path := LabelUnknown
+		if norm, ok := this.Normalizer(); ok {
+			path = xfb.OnNilv(norm(req), LabelUnknown)
+		}
+		ls["path"] = path
 
-			return ls
-		},
-		newFinishLabels: func(this roundTripWrapCfg, req *http.Request, resp *http.Response, err error) prometheus.Labels {
-			ls := prometheus.Labels{
-				"host":   req.URL.Hostname(),
-				"method": req.Method,
-			}
+		return ls
+	}
+	DefaultFinishLabelsFn = func(this RoundTripWrapConfig, req *http.Request, resp *http.Response, err error) prometheus.Labels {
+		ls := prometheus.Labels{
+			"host":   req.URL.Hostname(),
+			"method": req.Method,
+		}
 
-			path := LabelUnknown
-			if this.norm != nil {
-				path = xfb.OnNilv(this.norm(req), LabelUnknown)
-			}
-			ls["path"] = path
+		path := LabelUnknown
+		if norm, ok := this.Normalizer(); ok {
+			path = xfb.OnNilv(norm(req), LabelUnknown)
+		}
+		ls["path"] = path
 
-			code := LabelNoValue
-			if err == nil {
-				code = strconv.Itoa(resp.StatusCode)
-			}
-			ls["code"] = code
-			return ls
-		},
+		code := LabelNoValue
+		if err == nil {
+			code = strconv.Itoa(resp.StatusCode)
+		}
+		ls["code"] = code
+		return ls
 	}
 )
